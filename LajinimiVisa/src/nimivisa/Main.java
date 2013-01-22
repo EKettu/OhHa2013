f637f3423c68edd436881efd53efb7a3c383a5c9
Elina K <elina.kettunen@helsinki.fi>
2013-01-22 15:37:55 +0200

    projektia taas

--- LajinimiVisa/src/nimivisa/Main.java
+++ LajinimiVisa/src/nimivisa/Main.java
@@ -14,23 +14,10 @@ public class Main {
      */
     public static void main(String[] args) throws FileNotFoundException {
         
-      //s
-       // NimiVisa visa = new NimiVisa();
         TiedostonValinta valinta = new TiedostonValinta();
         
         TekstiKayttoliittyma liittyma = new TekstiKayttoliittyma(valinta);
         liittyma.kaynnista();
-
-//        Scanner skanneri = new Scanner(System.in);
-//
-//        TekstiKayttoliittyma kayttis = new TekstiKayttoliittyma(skanneri);
-//        kayttis.kaynnista();
-//
-//        TiedostonLukija lukija = new TiedostonLukija(new File(kayttis.getValittu()));
-//ss
-//        lukija.jaaRivitOsiin();
-
- //     System.out.println(lukija.getRiviLkm());
 
     }
 }
